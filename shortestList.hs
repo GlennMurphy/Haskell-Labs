43d6da410e1d2fecaa0a5d4c872512d4dee4f060
Glenn Murphy <glenn.murphy43@mail.dcu.ie>
2013-10-29 16:44:23 +0000

    Create shortestList.hs
    I feel like this could of been done better.

---
+++ shortestList.hs
@@ -0,0 +1,11 @@
+getelementofsize :: Int -> [[x]] -> [x]
+getelementofsize _ [] = []
+getelementofsize num (x:xs) =   if num == length x
+				then x
+				else getelementofsize num xs
+
+shortest :: [[a]] -> [a]
+shortest x = let minlist = minimum (map (length) x)
+		          in getelementofsize minlist x
+
+
