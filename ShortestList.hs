ea1ffec9fe8510ac5ad1cf46001ae937995433ea
Glenn Murphy <glenn.murphy43@mail.dcu.ie>
2013-10-29 16:46:48 +0000

    Update ShortestList.hs

--- ShortestList.hs
+++ ShortestList.hs
@@ -3,8 +3,9 @@ getelementofsize _ [] = []
 getelementofsize num (x:xs) =   if num == length x
 				then x
 				else getelementofsize num xs
+				
 shortest :: [[a]] -> [a]
-shortest x = 			let minlist = minimum (map (length) x)
+shortest x = 		let minlist = minimum (map (length) x)
 		          	in getelementofsize minlist x
 
 
