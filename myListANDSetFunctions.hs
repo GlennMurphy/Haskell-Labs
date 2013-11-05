aa6215ec3593a0b050c5c6aa1121b4e0e2532e8d
Glenn Murphy <glenn.murphy43@mail.dcu.ie>
2013-11-05 15:48:03 +0000

    Update and rename myListfunctions.hs to myListANDSetFunctions.hs
    Added set functions.

--- myListANDSetFunctions.hs
+++ myListANDSetFunctions.hs
@@ -1,8 +1,8 @@
 --	My HASKELL list functions. Lab 4.
---  Answers to Q1.
 
 myAppend :: [a] -> [a] -> [a]
-myAppend x y = x ++ y
+myAppend [] y = y
+myAppend (x:xs) y = x:myAppend xs y
 
 myHead :: [a] -> a
 myHead (x:xs) = x
@@ -16,7 +16,7 @@ myTail (x:xs) = xs
 
 myInit :: [a] -> [a]
 myInit [x] = []
-myInit (x:xs) = [x] ++ myInit xs
+myInit (x:xs) = myAppend [x] (myInit xs)
 
 myLength :: [a] -> Int
 myLength [] = 0
@@ -24,7 +24,7 @@ myLength (x:xs) = 1 + myLength(xs)
 
 myReverse :: [a] -> [a]
 myReverse [] = []
-myReverse (x:xs) = myReverse(xs) ++ [x]
+myReverse (x:xs) = myAppend (myReverse xs) [x]
 
 myConcat :: [[a]] -> [a]
 myConcat [] = []
@@ -53,6 +53,7 @@ myMinimum (x:xs)
 			where minTail = myMinimum xs
 			
 myElem :: Eq a => a -> [a] -> Bool
+myElem a [] = False
 myElem a [x] = a == x
 myElem a (x:xs)
 			| a == x = a == x
@@ -63,3 +64,20 @@ myDelete a [] = []
 myDelete a (x:xs)
 			| a == x = xs
 			| otherwise = x:myDelete a xs
+
+-- Answers to Q2.
+
+myUnion :: Eq a => [a] -> [a] -> [a] 
+
+myUnion a [] = a
+
+myUnion (x:xs) (y:ys)
+			| myElem x (y:ys) = myUnion (x:xs) (myDelete x (y:ys))
+			| myElem y (ys) = myUnion (x:xs) (myDelete y (y:ys))
+			| otherwise = [x] ++ myUnion ((xs) ++ [y]) ys
+			
+myIntersect :: Eq a => [a] -> [a] -> [a]
+myIntersect [] y = []
+myIntersect (x:xs) (y:ys)
+			| myElem x (y:ys) = myAppend [x] (myIntersect xs (y:ys))
+			| otherwise = myIntersect xs (y:ys)
